a298e24fc40b35cbd83fadb4320d81c349904e9c
Younes Tsouli <younestsouli444@gmail.com>
2026-02-14 21:09:00 +0100

    fix: fixing the links

--- src/components/welcomePage/header.jsx
+++ src/components/welcomePage/header.jsx
@@ -9,7 +9,7 @@ export default function Header() {
                 <p id="description">Creative Developer & Designer crafting beautiful digital experiences</p>
                 <div id="buttons">
                     <a href={"#projects-container"} id="projects">View my work</a>
-                    <a href={"#contact-me"} id="contact">Get in touch</a>
+                    <a href={"#contact-container"} id="contact">Get in touch</a>
                 </div>
             </div>
         </div>
